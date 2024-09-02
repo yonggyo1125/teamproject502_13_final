f408b47a217ae958d225f2b4664812c94e97382d
yonggyo1125 <yonggyo00@kakao.com>
2024-09-03 08:50:07 +0900

    .

--- member-service/src/main/java/org/choongang/global/configs/SwaggerConfig.java
+++ member-service/src/main/java/org/choongang/global/configs/SwaggerConfig.java
@@ -1,13 +1,12 @@
 package org.choongang.global.configs;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(info=@Info(title="회원인증 API", description = "회원 가입 및 인증 토큰 발급", contact = @Contact(name="Yonggyo, Lee", email="yonggyo00@kakao.com"), version="v1"))
+@OpenAPIDefinition(info=@Info(title="회원인증 API", description = "회원 가입 및 인증 토큰 발급"))
 @Configuration
 public class SwaggerConfig {
 
