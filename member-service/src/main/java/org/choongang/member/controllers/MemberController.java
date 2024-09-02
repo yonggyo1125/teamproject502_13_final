bb8d548d16eec19a393c9340812995c8ec356e00
yonggyo1125 <yonggyo00@kakao.com>
2024-09-03 08:44:17 +0900

    .

--- member-service/src/main/java/org/choongang/member/controllers/MemberController.java
+++ member-service/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,8 +1,13 @@
 package org.choongang.member.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.choongang.global.Utils;
 import org.choongang.global.exceptions.BadRequestException;
 import org.choongang.global.rests.JSONData;
@@ -18,7 +23,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
+@Tag(name = "Member", description = "회원 인증 API")
 @RestController
 @RequestMapping("/account")
 @RequiredArgsConstructor
@@ -29,6 +34,8 @@ public class MemberController {
     private final TokenProvider tokenProvider;
     private final Utils utils;
 
+    @Operation(summary = "인증(로그인)한 회원 정보 조회")
+    @ApiResponse(responseCode = "200")
     // 로그인한 회원 정보 조회
     @GetMapping
     @PreAuthorize("isAuthenticated()")
@@ -38,6 +45,16 @@ public class MemberController {
         return new JSONData(member);
     }
 
+    @Operation(summary = "회원가입")
+    @ApiResponse(responseCode = "201", description = "회원가입 성공시 201")
+    @Parameters({
+            @Parameter(name="email", required = true, description = "이메일"),
+            @Parameter(name="password", required = true, description = "비밀번호"),
+            @Parameter(name="confirmPassword", required = true, description = "비밀번호 확인"),
+            @Parameter(name="userName", required = true, description = "사용자명"),
+            @Parameter(name="mobile", description = "휴대전화번호, 형식 검증 있음"),
+            @Parameter(name="agree", required = true, description = "회원가입약관 동의")
+    })
     @PostMapping
     public ResponseEntity join(@RequestBody @Valid RequestJoin form, Errors errors) {
 
@@ -52,7 +69,13 @@ public class MemberController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "인증 및 토큰 발급", description = "인증 성공시 JWT 토큰 발급")
+    @ApiResponse(responseCode = "201", headers = @Header(name="application/json"), description = "data이 발급 받은 토큰")
 
+    @Parameters({
+        @Parameter(name="email", required = true, description = "이메일"),
+            @Parameter(name="password", required = true, description = "비밀번호")
+    })
     @PostMapping("/token")
     public JSONData token(@RequestBody @Valid RequestLogin form, Errors errors) {
 
@@ -64,6 +87,4 @@ public class MemberController {
 
         return new JSONData(token);
     }
-
-
 }
