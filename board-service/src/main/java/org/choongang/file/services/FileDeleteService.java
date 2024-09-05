b608b09b9b64242e24695893604393800780f4f8
yonggyo1125 <yonggyo00@kakao.com>
2024-09-06 08:59:30 +0900

    board-service 작업 중

--- board-service/src/main/java/org/choongang/file/services/FileDeleteService.java
+++ board-service/src/main/java/org/choongang/file/services/FileDeleteService.java
@@ -1,10 +1,12 @@
 package org.choongang.file.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.RequiredArgsConstructor;
 import org.choongang.file.entities.FileInfo;
 import org.choongang.global.rests.ApiRequest;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -25,6 +27,14 @@ public class FileDeleteService {
 
 
     public List<FileInfo> delete(String gid, String location) {
+        String url = "/deletes/" + gid;
+        if (StringUtils.hasText(location)) url += "&location=" + location;
+
+        ApiRequest result = apiRequest.request(url, "file-service", HttpMethod.DELETE);
+
+        if (result.getStatus().is2xxSuccessful() && result.getData().isSuccess()) {
+            return result.toJSON(new TypeReference<>() {});
+        }
 
         return null;
     }
