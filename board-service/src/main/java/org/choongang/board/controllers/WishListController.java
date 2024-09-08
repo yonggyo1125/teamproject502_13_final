5e89dd5b1aef7e4095d95d75b653d1d6bf5cb335
yonggyo1125 <yonggyo00@kakao.com>
2024-09-09 08:20:30 +0900

    board-service 게시판 관리 API

--- board-service/src/main/java/org/choongang/board/controllers/WishListController.java
+++ board-service/src/main/java/org/choongang/board/controllers/WishListController.java
@@ -1,5 +1,9 @@
 package org.choongang.board.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.choongang.board.services.WishListService;
 import org.choongang.global.rests.JSONData;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name="Wish", description = "찜하기 API")
 @RestController
 @RequestMapping("/wish")
 @RequiredArgsConstructor
@@ -16,6 +21,8 @@ public class WishListController {
 
     private final WishListService service;
 
+    @Operation(summary = "찜한 게시글 번호 목록 조회", method = "GET")
+    @ApiResponse(responseCode = "200")
     @GetMapping("/list")
     public JSONData list() {
         List<Long> seqs = service.getList();
@@ -23,6 +30,9 @@ public class WishListController {
         return new JSONData(seqs);
     }
 
+    @Operation(summary = "게시글 찜하기", method="GET")
+    @ApiResponse(responseCode = "201")
+    @Parameter(name="seq", required = true, description = "경로변수, 게시글 등록 번호")
     // 추가
     @GetMapping("/{seq}")
     public ResponseEntity<Void> add(@PathVariable("seq") Long seq) {
@@ -32,6 +42,10 @@ public class WishListController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+
+    @Operation(summary = "게시글 찜하기 해제", method="DELETE")
+    @ApiResponse(responseCode = "200")
+    @Parameter(name="seq", required = true, description = "경로변수, 게시글 등록 번호")
     // 추가
     @DeleteMapping("/{seq}")
     public ResponseEntity<Void> remove(@PathVariable("seq") Long seq) {
